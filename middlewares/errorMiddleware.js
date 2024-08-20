e6f72445eb2c397cd90f74884e34c56ad0cc0360
mr-mashanlo <mr.mashanlo@yandex.ru>
2024-08-20 11:31:35 +0500

    refactor error middleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -5,7 +5,7 @@ const errorMiddleware = ( err, req, res, next ) => {
   if ( err instanceof GeneralError ) {
     return res.status( err.getCode() ).json( { code: err.code, message: err.message } );
   }
-  return res.status( 500 ).json( { message: err.message } );
+  return res.status( 500 ).json( { code: err.code, message: err.message } );
 };
 
 module.exports = errorMiddleware;
